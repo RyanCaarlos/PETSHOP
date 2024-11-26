847d10def17653ee5597677cb0f0736715ad36b2
RyanCaarlos <167880048+RyanCaarlos@users.noreply.github.com>
2024-11-26 08:52:49 -0300

    index.php

--- index.php
+++ index.php
@@ -21,7 +21,23 @@
     </div>
 </header>
 <body>
-    <img class="fundo" src="img/Vector.png" alt="fundo verde"/>
-    <img class="img" src="img/Pexels_Photo_by_Charles-removebg-preview 1.png" alt="cachorro">
+<img class="fundo" src="img/Vector.png" alt="fundo verde"/>
+<img class="img" src="img/Pexels_Photo_by_Charles-removebg-preview 1.png" alt="cachorro">
+ 
+<?php
+
+    include_once("templates/topo.php");
+    include_once("templates/menu.php");
+
+    if(empty($_SERVER["QUERY_STRING"])){
+        $var = "principal.php";
+        include_once($var);
+    }else{
+        $pg = $_GET["pg"];
+        include_once("$pg.php");
+    }
+
+    include_once("templates/rodape.php");
+?>
 </body>
 </html>
