158a45459095d018e7886489cfa4edcad9a74cb9
RyanCaarlos <167880048+RyanCaarlos@users.noreply.github.com>
2024-11-26 08:57:29 -0300

    menu

--- index.php
+++ index.php
@@ -10,9 +10,6 @@
     </style>
 </head>
 <body>
-<img class="fundo" src="img/Vector.png" alt="fundo verde"/>
-<img class="img" src="img/Pexels_Photo_by_Charles-removebg-preview 1.png" alt="cachorro">
- 
 <?php
 
     include_once("templates/topo.php");
