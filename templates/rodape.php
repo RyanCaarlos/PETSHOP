93880a7647ac81dda01d1e80159e0b40d74182d0
Annanda <165848254+AnnandaV@users.noreply.github.com>
2024-11-27 09:48:45 -0300

    Update rodape.php

---
+++ templates/rodape.php
@@ -0,0 +1,9 @@
+<footer>
+    <img src="img/Top navigation bar.png" alt="logo" class="logo-footer">
+    <p>Quer ficar por dentro das novidades <br> 
+        inovadoras e exclusivas, e ainda ver <br>
+        conteúdos incríveis? Então siga-nos <br>
+        nas redes sociais!
+    </p>
+    <h6>Contate-nos: jardimpet@gmail.com</h6>
+</footer>
