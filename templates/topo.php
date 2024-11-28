67e764a6e5859002e1b70c854d84b7dfa3ce623d
RyanCaarlos <167880048+RyanCaarlos@users.noreply.github.com>
2024-11-28 09:52:48 -0300

    topo update

--- templates/topo.php
+++ templates/topo.php
@@ -5,9 +5,17 @@
         </div>
     </div>
     <div class="container">
-        <div class="botoes">
-            <a href="login_adm.php">Administrador</a>
-        </div>
-        <div class="botoes">Login</div>
+        <div class="botoes" onclick="redirectToAdminLogin()">Administrador</div>
+        <div class="botoes" onclick="redirectToLogin()">Login</div>
     </div>
 </header>
+
+<script>
+function redirectToAdminLogin() {
+    window.location.href = 'login_adm.php';
+}
+
+function redirectToLogin() {
+    window.location.href = 'login.php'; // Assuming you have a login.php file
+}
+</script>
